16c6a79e247dd60b8766dd576287243b337242f2
naino <Hasnainkhan21>
2025-08-06 17:27:09 +0500

    cors setup

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ const UserRoutes = require('./Router/portfolioRoutes');
 const path = require('path');
 
 connectDB();
-app.use(cors());
+const allowedOrigins = ['https://intern-portfolio-rouge.vercel.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/api/v0/users', UserRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'Uploads')));
