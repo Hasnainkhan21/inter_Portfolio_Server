d2ddc79643de4e7e7e62f7a4c3d853cb820a449f
naino <Hasnainkhan21>
2025-10-03 23:01:03 +0500

    sdc

--- server.js
+++ server.js
@@ -23,6 +23,15 @@ app.use(
 
 app.use(express.json());
 
+// Add this before your routes
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://intern-portfolio-rouge.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 // Root route
 app.get("/", (req, res) => {
   res.json({ status: "API is running" });
